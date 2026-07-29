13e3200f26c08a4f2203c4db10a53dac3291f718
chatbotproject <madhavimadhu2655@gmail.com>
2026-07-29 12:58:08 +0530

    first commit

---
+++ TNSIF_Training_Java/src/org/tnsif/acc/c2tc/methodoverriding/SuperWithMethod.java
@@ -0,0 +1,28 @@
+package org.tnsif.acc.c2tc.methodoverriding;
+
+class Payment1{
+	void process() {
+		System.out.println("Processing payment using standard gateway");
+	}
+}
+
+class Gpay extends Payment1{
+	void process() {
+		System.out.println("Processing payment using via Google Pay");
+		
+	}
+	void completeTransaction() {
+		super.process();
+		process();
+	}
+}
+
+public class SuperWithMethod {
+
+	public static void main(String[] args) {
+		Gpay obj=new Gpay();
+		obj.completeTransaction();
+
+	}
+
+}
