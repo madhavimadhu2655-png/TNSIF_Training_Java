27e474cc7478307b6fc778b697b016ab88fd9342
chatbotproject <madhavimadhu2655@gmail.com>
2026-08-31 11:02:12 +0530

    first commit

---
+++ TNSIF_Training_Java/src/org/tnsif/acc/c2tc/generics/LowerBound.java
@@ -0,0 +1,10 @@
+package org.tnsif.acc.c2tc.generics;
+
+public class LowerBound {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ TNSIF_Training_Java/src/org/tnsif/acc/c2tc/generics/UpperBounded.java
@@ -0,0 +1,10 @@
+package org.tnsif.acc.c2tc.generics;
+
+public class UpperBounded {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
