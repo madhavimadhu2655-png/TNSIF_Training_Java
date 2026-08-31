c757e5035cc6ad6a02dbe724727044bf8d4b825e
chatbotproject <madhavimadhu2655@gmail.com>
2026-08-31 11:02:34 +0530

    first commit

--- TNSIF_Training_Java/src/org/tnsif/acc/c2tc/generics/ArrayList.java
+++ TNSIF_Training_Java/src/org/tnsif/acc/c2tc/generics/ArrayList.java
@@ -1,9 +1,19 @@
 package org.tnsif.acc.c2tc.generics;
 
+
+import java.util.List;
+
 public class ArrayList {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		List list=(List) new ArrayList();
+		list.add("abc");
+		list.add(new Integer(5));
+		for(Object obj : list){
+			//type casting leading to ClassCastException at runtime
+		    String str=(String) obj; 
+		}
+
 
 	}
 
